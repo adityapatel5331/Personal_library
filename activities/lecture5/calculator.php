4edbb61e54388e5b4830d050c1847d042228b962
Aditya Piyush Patel <ad446247@dal.ca>
2023-09-28 00:09:00 -0300

    Replace calculator.php

--- activities/lecture5/calculator.php
+++ activities/lecture5/calculator.php
@@ -1,40 +1,38 @@
 <?php
-function calculate($number1, $number2, $operator)
-{
-    $result = '';
+$number1 = $_POST['number1'];
+$number2 = $_POST['number2'];
+$operator = $_POST['operator'];
+$result = '';
 
-    if (is_numeric($number1) && is_numeric($number2)) {
-        switch ($operator) {
-            case 'Add':
-                $result = $number1 + $number2;
-                break;
-            case 'Subtract':
-                $result = $number1 - $number2;
-                break;
-            case 'Multiply':
-                $result = $number1 * $number2;
-                break;
-            case 'Divide':
-                if ($number2 != 0) {
-                    $result = $number1 / $number2;
-                } else {
-                    $result = 'Cannot divide by zero.';
-                }
-                break;
-            case 'Even':
-                if ($number1 % 2 == 0) {
-                    $result = 'Even';
-                } else {
-                    $result = 'Odd';
-                }
-                break;
-            default:
-                $result = 'Invalid operator.';
-        }
-    } else {
-        $result = 'You have not entered valid numeric values.';
+if (is_numeric($number1) && is_numeric($number2)) {
+    switch ($operator) {
+        case 'Add':
+            $result = $number1 + $number2;
+            break;
+        case 'Subtract':
+            $result = $number1 - $number2;
+            break;
+        case 'Multiply':
+            $result = $number1 * $number2;
+            break;
+        case 'Divide':
+            if ($number2 != 0) {
+                $result = $number1 / $number2;
+            } else {
+                $result = '<strong>Error: Division by zero</strong>';
+            }
+            break;
+        case 'Modulus':
+            if ($number2 != 0) {
+                $result = $number1 % $number2;
+            } else {
+                $result = '<strong>Error: Modulus by zero</strong>';
+            }
+            break;
+        default:
+            $result = '<strong>Invalid operator</strong>';
     }
-
-    return $result;
+} else {
+    $result = '<strong>You have not entered valid numbers</strong>';
 }
 ?>
