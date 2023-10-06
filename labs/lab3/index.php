bf7fcc4064e12596e41556802f778b2e18213837
Aditya Piyush Patel <ad446247@dal.ca>
2023-10-05 23:25:24 -0300

    Replace index.php

--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -1,22 +1,29 @@
 <?php include 'includes/header.php'; ?>
 
 <script>
+    // function validateForm will validate the form.
+
     function validateForm() {
+        // we will enter the value from different boxes in the assigned variable
+
         var firstName = document.getElementById("firstName").value;
         var lastName = document.getElementById("lastName").value;
         var email = document.getElementById("email").value;
         var password = document.getElementById("password").value;
         var confirmPassword = document.getElementById("confirmPassword").value;
+        //use of regex expression 
 
         var nameRegex = /^[a-zA-Z]+( [a-zA-Z]+)?$/; // Allow space for middle name
         var lastNameRegex = /^[a-zA-Z'-]+$/; // Allow apostrophe and hyphen in last name
         var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
         var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{12,}$/;
+        // storing the errors from different validation inside the variables
 
         var firstNameError = document.getElementById("first-name-error");
         var lastNameError = document.getElementById("last-name-error");
         var emailError = document.getElementById("email-error");
         var passwordError = document.getElementById("password-error");
+        //  firstly the error content for every category will be an empty string 
 
         firstNameError.textContent = "";
         lastNameError.textContent = "";
@@ -24,6 +31,7 @@
         passwordError.textContent = "";
 
         var isValid = true;
+        // if there is any issue, isValid will become false;
 
         if (!firstName.match(nameRegex)) {
             firstNameError.textContent = "First name must contain only letters and can include a space for middle name.";
@@ -64,7 +72,7 @@
 </script>
 
 <body>
-    <h1>Welcome to the index page</h1>
+    <h1>Welcome to the index page </h1>
     <bold> If you want to move on the welcome page, Please fill the form given below!!!<bold>
             <br><br><br>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed totam illo dolorem! Nostrum quos eius
