b6491ebfef2028e3b05bf4dae0375d75bcbad6e6
Aditya Piyush Patel <ad446247@dal.ca>
2023-10-15 21:42:50 -0300

    Upload New File

---
+++ labs/lab4/welcome.php
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+
+    Welcome
+    <?php echo $_POST["name"]; ?><br>
+    Your email address is:
+    <?php echo $_POST["email"]; ?>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quidem aspernatur soluta voluptatibus voluptates
+    consequuntur hic ut ab nobis esse delectus autem error maiores fuga enim, commodi asperiores debitis! Labore!
+</body>
+
+</html>
