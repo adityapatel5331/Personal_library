ede4d3052b557b2f3129b2d6cbd8c8e4fcb33cd3
Aditya <adityapatel5331@gmail.com>
2023-11-18 16:57:51 -0400

    l20 done

--- labs/lab3/Lab3_Patel_Aditya_README.txt
+++ labs/lab3/Lab3_Patel_Aditya_README.txt
@@ -1,6 +1,8 @@
 # Lab 3
 
-* Date Created: 01 10 2023
+(Lecture 20 Activity Re-submission)
+
+* Date Created: 18 11 2023
 * Last Modification Date: 04 10 2023
 * Lab URL: https://web.cs.dal.ca/~patel29/csci2170/lab3/
 * Git URL: https://git.cs.dal.ca/patel29/csci2170/-/tree/main/labs/lab3
@@ -12,9 +14,14 @@
 
 ## Getting Started
 
+For this activity I am re submitting the lab 3 in which I have received 6/10. 
+Feedback provided for lab3 by marker:- (Good Attempt but the use of JS instead of php is not technically how this would be done if this was CSCI3172 then it would work but in the event of this course not entirely.
 
+Marked by: Dave Chuck / dchuck@dal.ca )
 
+From the provided feedback I think, we just have to correct the mistake and change those by learning from our mmistakes. And the feedback will be working as our rubric and Thus I believe that my work is complete except for the part inw which I have used JS instead of PHP. 
 
+So those part will be updated and rest all will remain same.
 ### Prerequisites
 
 No Prerequisites are allowed to view the project
--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -1,101 +1,15 @@
-<?php include 'includes/header.php'; ?>
+<?php include 'header.php'; ?>
 
-<script>
-    // function validateForm will validate the form.
-
-    function validateForm() {
-        // we will enter the value from different boxes in the assigned variable
-
-        var firstName = document.getElementById("firstName").value;
-        var lastName = document.getElementById("lastName").value;
-        var email = document.getElementById("email").value;
-        var password = document.getElementById("password").value;
-        var confirmPassword = document.getElementById("confirmPassword").value;
-        //use of regex expression 
-
-        var nameRegex = /^[a-zA-Z]+( [a-zA-Z]+)?$/; // Allow space for middle name
-        var lastNameRegex = /^[a-zA-Z'-]+$/; // Allow apostrophe and hyphen in last name
-        var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
-        var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{12,}$/;
-        // storing the errors from different validation inside the variables
-
-        var firstNameError = document.getElementById("first-name-error");
-        var lastNameError = document.getElementById("last-name-error");
-        var emailError = document.getElementById("email-error");
-        var passwordError = document.getElementById("password-error");
-        //  firstly the error content for every category will be an empty string 
-
-        firstNameError.textContent = "";
-        lastNameError.textContent = "";
-        emailError.textContent = "";
-        passwordError.textContent = "";
-
-        var isValid = true;
-        // if there is any issue, isValid will become false;
-
-        if (!firstName.match(nameRegex)) {
-            firstNameError.textContent = "First name must contain only letters and can include a space for middle name.";
-            isValid = false;
-        }
-
-        if (!lastName.match(lastNameRegex)) {
-            lastNameError.textContent = "Last name must contain only letters, apostrophes, or hyphens.";
-            isValid = false;
-        }
-
-        if (!email.match(emailRegex)) {
-            emailError.textContent = "Invalid email address.";
-            isValid = false;
-        }
-
-        if (!password.match(passwordRegex)) {
-            passwordError.textContent = "Password must be at least 12 characters long and include one digit, one lowercase letter, one uppercase letter, and one special character.";
-            isValid = false;
-        }
-
-        if (password.trim() === "") {
-            passwordError.textContent = "Password is required.";
-            isValid = false;
-        }
-
-        if (confirmPassword.trim() === "") {
-            passwordError.textContent = "Confirm Password is required.";
-            isValid = false;
-        } else if (password !== confirmPassword) {
-            passwordError.textContent = "Passwords do not match.";
-            isValid = false;
-        }
-
-        return isValid;
-    }
-
-</script>
+<?php include './validate.php'; ?>
 
 <body>
+
     <h1>Welcome to the index page </h1>
     <bold> If you want to move on the welcome page, Please fill the form given below!!!<bold>
             <br><br><br>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed totam illo dolorem! Nostrum quos eius
-            consectetur beatae, quidem maiores laudantium. Possimus perferendis qui suscipit corporis eligendi at
-            blanditiis, quos ipsa ab molestias labore cupiditate perspiciatis vel nemo nostrum esse laudantium, hic
-            minus similique dolore doloremque placeat velit. Aliquam, deleniti doloremque?
-            <div class="form-container">
-                <form action="welcome.php" method="post" onsubmit="return validateForm()">
-                    First Name: <input type="text" name="firstName" id="firstName"><br>
-                    <span class="error-message" id="first-name-error"></span><br>
-                    Last Name: <input type="text" name="lastName" id="lastName"><br>
-                    <span class="error-message" id="last-name-error"></span><br>
-                    E-mail: <input type="text" name="email" id="email"><br>
-                    <span class="error-message" id="email-error"></span><br>
-                    Password: <input type="password" name="password" id="password"><br>
-                    Confirm Password: <input type="password" name="confirmPassword" id="confirmPassword"><br>
-                    <span class="error-message" id="password-error"></span><br>
-                    <input type="submit">
-                </form>
-            </div>
+            <?php include './form.php'; ?>
 </body>
 
+<?php include './footer.php' ?>
 
 </html>
-
-<?php include 'includes/footer.php'; ?>
