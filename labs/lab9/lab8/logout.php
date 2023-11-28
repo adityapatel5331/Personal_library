7c811660d7c3cd4b1c75a721a8d51891c38e1310
Aditya <adityapatel5331@gmail.com>
2023-11-28 14:45:34 -0400

    extra files removed

--- labs/lab9/lab8/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['username']);
-$username = '';
-
-header("Location: index.php");
-exit();
-?>
--- labs/lab9/lab8/logoutButton.php
+++
@@ -1,5 +0,0 @@
-<div class="form-container text-right">
-    <form method="post" action="logout.php">
-        <button type="submit" class="btn btn-secondary">Logout</button>
-    </form>
-</div>
